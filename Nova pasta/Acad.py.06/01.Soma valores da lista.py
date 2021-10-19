15e8e349b45b18f7020fdd85a28a0364037f4f87
pedro ivo <92477551+pedroivoal@users.noreply.github.com>
2021-10-19 20:05:15 -0300

    Add files via upload

---
+++ Nova pasta/Acad.py.06/01.Soma valores da lista.py
@@ -0,0 +1,7 @@
+def soma_valores(lista):
+    i = 0
+    s = 0
+    while i < len(lista):
+        s += lista[i]
+        i += 1
+    return s
---
+++ Nova pasta/Acad.py.06/06.Norma de um vetor.py
@@ -0,0 +1,5 @@
+def calcula_norma(x, y):
+    import math
+
+    norma = math.sqrt(x**2 + y**2)
+    return norma
