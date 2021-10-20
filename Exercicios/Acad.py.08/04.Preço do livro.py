aba26e5de20a8c9c20b34a08923d232021b4922f
pedro ivo <92477551+Pedro-Ivo-1@users.noreply.github.com>
2021-10-20 16:37:40 -0300

    Create 04.Preço do livro.py

---
+++ Exercicios/Acad.py.08/04.Preço do livro.py
@@ -0,0 +1,10 @@
+def verifica_preco (titulo,dic_nome_cor,dic_cor_prec):
+
+    cor = dic_nome_cor[titulo]
+    prec = dic_cor_prec[cor]
+
+    return prec
+
+
+dicionário_de_livros: {"Pinóquio": "azul", "Dom Quixote": "amarelo", "O Pequeno Príncipe": "vermelho"}
+cores: {'vermelho': 10.0, 'azul': 20.0, 'amarelo': 40.0, "verde": 100.0 }
