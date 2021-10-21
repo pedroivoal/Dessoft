09ac357c9b30eef52379c346e5a460a0533dfa2b
pedro ivo <92477551+Pedro-Ivo-1@users.noreply.github.com>
2021-10-21 09:19:39 -0300

    Create 05.Média de notas.py

---
+++ Exercicios/Acad.py.08/05.Média de notas.py
@@ -0,0 +1,18 @@
+def calcula_media(lista_media_individual):
+    c = 0
+    soma = 0
+    for l in lista_media_individual:
+        
+        for d in l.values():
+            soma += d
+            c += 1
+
+    media = soma/c
+
+    return media
+
+lista = [ {"aluno1": 5, "aluno2": 8}, {"aluno3": 5} ]
+
+m = calcula_media(lista)
+
+print(m)
