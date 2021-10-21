e56f4c2e100fb1e4a5d5caa2d9028992c5985698
pedro ivo <92477551+Pedro-Ivo-1@users.noreply.github.com>
2021-10-21 14:54:12 -0300

    .

---
+++ Exercicios/Acad.py.08/06.Primeira ocorrência de cada caractere.py
@@ -0,0 +1,13 @@
+def primeiras_ocorrencias(palavra):
+
+    dic = {}
+
+    for letra in palavra:
+        p = palavra.find(letra)
+        dic[letra] = p
+
+    return dic
+
+dic_t = primeiras_ocorrencias('abracadabra')
+
+print(dic_t)
