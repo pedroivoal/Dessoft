05e6ab9d3cfd21f03d2a62e720564658ee89dcc0
Pedro Ivo <92477551+pedroivoal@users.noreply.github.com>
2021-12-08 13:26:01 -0300

    .

--- 1.Dúvidas.py
+++
@@ -1,4 +0,0 @@
-'''1) teria um jeito de fazer o 08. da Acad.py.08 mais facilmente?
-Eu tentei fazer somente com os dois primeiros for(s) mas não deu certo'''
-
-'''2) no código 20 da acad.py.04 fiz duas funçõe, foi melhor dessa forma do que fazer um código corrido? Como meu código poderia ficar mais organizado? Qual o significado do -0.0 para a máquina?'''
