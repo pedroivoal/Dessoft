31bce8f238afce38bc42e2ddacf5aab81cd6ae52
Pedro Ivo <92477551+pedroivoal@users.noreply.github.com>
2021-12-08 15:41:14 -0300

    .

--- Testes/PF/04.Classificação por vizinho mais próximo.py
+++ Testes/PF/04.Classificação por vizinho mais próximo.py
@@ -3,18 +3,20 @@ def acha_distancia(x1, y1, x2, y2):
     return d
 
 def classifica(D_fish, dim):
-    menor = 1000
+    menor = 10000
     x1 = dim[0]
     y1 = dim[1]
 
     for k in D_fish:
         size = len(D_fish[k])
 
+        # Percorre cada dimeção de peixe conhecido
         for i in range(0, size):
             x2 = D_fish[k][i][0]
             y2 = D_fish[k][i][1]
             d = acha_distancia(x1, y1, x2, y2)
 
+            # Atualiza menor distância 
             if d < menor:
                 r = k
                 menor = d
