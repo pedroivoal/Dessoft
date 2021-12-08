5264b38a06fbdf3674478e26d251e7d0ea04b1aa
Pedro Ivo <92477551+pedroivoal@users.noreply.github.com>
2021-12-08 15:08:17 -0300

    .

---
+++ Testes/PF/03.Bingo de Palavras.py
@@ -0,0 +1,31 @@
+def verifica_ganhador(text, D_players):
+    text = text.lower()
+
+    D_return = {}
+
+    for k in D_players:
+        D_return[k] = 0
+
+        for p in D_players[k]:
+            points = 0
+            points += text.split().count(p + ',')
+            points += text.split().count(p + '.')
+            points += text.split().count(p + ':')
+            points += text.split().count(p + ';')
+            points += text.split().count(p)
+            D_return[k] += points
+    
+    return D_return
+
+string = '''Algum tempo hesitei se devia abrir estas memórias pelo princípio ou pelo fim, isto é, 
+se poria em primeiro lugar o meu nascimento ou a minha morte. Suposto o uso vulgar seja começar pelo 
+nascimento, duas considerações me levaram a adotar diferente método: a primeira é que eu não sou propriamente 
+um autor defunto, mas um defunto autor, para quem a campa foi outro berço; a segunda é que o escrito ficaria 
+assim mais galante e mais novo. Moysés, que tambem contou a sua morte, não a poz no introito, mas no cabo: 
+diferença radical entre este livro e o Pentateuco.'''
+D = {
+    'joao': ['paz', 'memórias', 'fim'],
+    'maria': ['defunto', 'livro', 'felicidade']
+}
+
+print(verifica_ganhador(string, D))
---
+++ lixo.py
@@ -0,0 +1,12 @@
+# text = text.lower()
+#     vi = text.find(',')
+#     po = text.find('.')
+#     po2 = text.find(':')
+
+#     while vi != 0 or po != 0 or po2 != 0:
+#         text[vi].replace('')
+#         text[po].replace('')
+#         text[po2].replace('')
+#         vi = text.find(',')
+#         po = text.find('.')
+#         po2 = text.find(':')
