e88fff0b1ecdb94f6b1347e08c18e993c9927c26
Pedro Ivo <92477551+pedroivoal@users.noreply.github.com>
2021-12-11 12:38:57 -0300

    .

--- Exercicios/02.Função/11.area_pentagono.py
+++ Exercicios/02.Função/11.area_pentagono.py
@@ -1,11 +1,10 @@
-# 
-import math
+from math import *
 
 def area_pentagono(l):
-    ang = math.radians(54)
-    area = 2.5 * l * l * math.tan(ang) / 2
+    ang = radians(54)
+    area = 2.5 * l * l * tan(ang) / 2
     return area
 
 
-print (math.radians(54))
-print(math.tan(math.radians(54)))
+print (radians(54))
+print(tan(radians(54)))
--- Exercicios/02.Função/13.encontra_uma_raiz.py
+++ Exercicios/02.Função/13.encontra_uma_raiz.py
@@ -1,5 +1,6 @@
-# 
 def encontra_uma_raiz(a, b, c):
+
     Δ = b*b - 4*a*c
     R = (-b + Δ**(1/2))/(2*a)
+    
     return R
