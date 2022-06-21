97a295dc14205890317407ba9b6be03f0c1273d3
Pedro Ivo <92477551+pedroivoal@users.noreply.github.com>
2022-06-21 15:57:42 -0300

    .

--- Exercicios/05.Laço/25.Pacotes no correio.py
+++ Exercicios/05.Laço/25.Pacotes no correio.py
@@ -1,2 +1,35 @@
-def pacotes_correio():
-    return 0
+
+def pacotes_correio(remessa):
+
+    ordem = []
+    tamanhos = []
+    numero = []
+
+    for i in remessa:
+        ordem.append(i[1])
+        tamanhos.append(i[2])
+        numero.append(i[0])
+
+    t_min = min(tamanhos)
+    t_max = max(tamanhos)
+    if t_max != t_min:
+        return 'tamanho errado'
+    
+    n = len(remessa)
+    n_min = min(numero)
+    n_max = max(numero)
+    if n_max != n_min or n_max!=n:
+        return 'pacote perdido'
+
+    ordinal = []
+    i = 1
+    while i <= len(remessa):
+        ordinal.append(i)
+        i += 1
+
+    if ordem != ordinal:
+        return 'ordem errada'
+    
+    return 'tudo certo'
+
+# print(pacotes_correio(remessa = [[4,1,20],[4,2,20],[4,3,20],[4,4,20]]))
