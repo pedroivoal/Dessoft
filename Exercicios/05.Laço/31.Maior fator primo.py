ca73159879b2b4c5546fea8cfd43fd6e5f1ec7a6
pedroivoal <pedroial@al.insper.edu.br>
2023-03-29 20:32:33 -0300

    .

--- Exercicios/05.Laço/31.Maior fator primo.py
+++ Exercicios/05.Laço/31.Maior fator primo.py
@@ -1,35 +1,30 @@
-def maior_fator_primo(num0):
+import time
 
-    num = num0
-    primos = []
-    if num%2==0:
-        primos.append(2)
-        while num%2==0:
-            num = int(num/2)
+# def maior_fator_primo(n):
+#     i = 2
+#     while n != 1:
+#         while n % i == 0:
+#             n = n / i
+#         i += 1
+#     return i - 1
 
+def maior_fator_primo(n):
+    while n % 2 == 0:
+        n = n / 2
+
+    if n == 1:
+        return 2
+    
     i = 3
-    while i < int(num/2)+1:
-
-        if num%i == 0:
-            
-            primos.append(i)
-            while num%i==0:
-                num = int(num/i)
+    while n != 1:
+        while n % i == 0:
+            n = n / i
         i += 2
+    return i - 2
 
-    # foram usadas listas no código. Para fazer sem listas bastaria ter uma variável (pmax) que guarda o maior primo
-    # e ao invés do while principal ter (i < int(num/h)+1) ter (i <= num) assim bastaria retornar a variavel pmax
-    # e o código abaixo se tornaria desnacessário.
-
-    # verifica se o próprio número não é primo
-    prod = 1
-    for p in primos:
-        prod *= p
-    if prod != num:
-        primos.append(num)
-
-    return max(primos)
-
-# print(maior_fator_primo(5*7*67*827*4040669))
+t0 = time.time()
+print(maior_fator_primo(5*7*67*827*4040669))
+tf = time.time()
+print(f'Tempo para achar maior fator primo: {(tf-t0)*1000:.2f} ms')
 # print(5*7*67*827*4040669)
 # print(maior_fator_primo(7836130001735))
